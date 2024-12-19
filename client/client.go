6f8f2a4ca94bd5cc84a603a6ff9cf97ac0bf7d24
hkeee2008 <hharmen777@gmail.com>
2024-12-20 01:17:02 +0300

    start development

--- client/client.go
+++ client/client.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+
+	"github.com/gorilla/websocket"
+)
+
+var addr = flag.String(os.Args[1], "localhost:"+os.Args[2], "http service address")
+
+func main() {
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/client"}
+	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Panicln("Error connection to server:", err)
+	}
+	fmt.Println(u.String())
+	defer conn.Close()
+
+	_, msg, err := conn.ReadMessage()
+
+	if err != nil {
+		log.Panicln("Error to read message from server:", err)
+	}
+
+	fmt.Println(string(msg))
+}
--- server/server.go
+++ server/server.go
@@ -1,13 +1,123 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{}
 
+type Client struct {
+	Name      string `json:"name"`
+	User_name string `json:"user_name"`
+	Ip_public string `json:"ip_public"`
+	Ip_local  string `json:"ip_local"`
+	OS        string `json:"os"`
+}
+
+type connClient struct {
+	Conn   *websocket.Conn
+	Client Client
+}
+
+type Clients struct {
+	clients map[int]*connClient
+	mu      sync.Mutex
+}
+
+type Admins struct {
+	admins map[int]*connClient
+	mu     sync.Mutex
+}
+
+var clients Clients
+
+var admins Admins
+
+func addClient(conn *websocket.Conn) (int, error) {
+	var client Client
+	err := conn.ReadJSON(&client)
+	if err != nil {
+		return 0, err
+	}
+	id := 1
+	var connClient connClient
+	connClient.Client = client
+	connClient.Conn = conn
+	clients.mu.Lock()
+	for {
+		if _, ok := clients.clients[id]; !ok {
+			clients.clients[id] = &connClient
+			break
+		}
+		id++
+	}
+	clients.mu.Unlock()
+	return id, nil
+}
+
+func removeClient(id int) {
+	clients.clients[id].Conn.Close()
+	clients.mu.Lock()
+	delete(clients.clients, id)
+	clients.mu.Unlock()
+
+}
+
 func admin(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func client(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Error to upgrade client connection:", err)
+		return
+	}
+
+	id, err := addClient(conn)
+
+	if err != nil {
+		conn.Close()
+		return
+	}
+
+	defer removeClient(id)
+
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+	ch1 := make(chan string)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				conn.SetWriteDeadline(time.Now().Add(5 * time.Second))
+				if err := conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+					ch1 <- "error"
+					return
+				}
+			}
+		}
+	}()
+
+	// ДОДЕЛАТЬ ПОЛУЧЕНИЯ РЕЗУЛЬТАТА ОТ КЛИЕНТА
+
+	select {
+	case <-ch1:
+		return
+	}
+
+}
+
+func main() {
+	http.HandleFunc("/admin", admin)
+	http.HandleFunc("/client", client)
+	err := http.ListenAndServe("127.0.0.1:8080", nil)
+	if err != nil {
+		log.Panicln("ListenAndServe err:", err)
+	}
 }
